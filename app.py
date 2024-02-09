9e7c3504f8f71c8dbb85e7205793c59ad08ea442
Sejal Dua <sejaldua@gmail.com>
2024-02-08 19:51:28 -0500

    init app

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+# Import packages
+from nba_api.stats.endpoints import shotchartdetail
+import json
+import requests
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Load teams file
+teams = json.loads(requests.get('https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json').text)
+# Load players file
+players = json.loads(requests.get('https://raw.githubusercontent.com/bttmly/nba/master/data/players.json').text)
+
+team_df = pd.DataFrame(teams)
+player_df = pd.DataFrame(players)
+player_df['playerName'] = player_df['firstName'] + (' ' * player_df.shape[0]) + player_df['lastName']
+
+team_list = team_df['teamName'].to_list()
+
+# Get team ID based on team name
+def get_team_id(t):
+    print(t)
+    for team in teams:
+        if team['teamName'] == t:
+            return team['teamId']
+    return -1
+
+# Get player ID based on player name
+def get_player_id(name):
+    for player in players:
+        if player['firstName'] + ' ' + player['lastName'] == name:
+            return player['playerId']
+    return -1
+
+TEAM = st.sidebar.selectbox('Select a team', team_list)
+tid = get_team_id(TEAM)
+
+PLAYER = st.sidebar.selectbox('Select a player', player_df[player_df['teamId'] == tid]['playerName'].to_list())
+pid = get_player_id(PLAYER)
+
+st.write('Hello')
